41d8187be7753c749463966a6aadc97007738559
he-lioenay <elioenay.perez@gmail.com>
2022-09-07 14:01:07 +0200

    borrar compiler.h

--- Paint/main.cpp
+++ Paint/main.cpp
@@ -2,13 +2,8 @@
 #include "CLI11.hpp"
 
 
-int main(int argc, char** argv){
-    CLI::App app{"App description"};
+int main(){
 
-    std::string filename = "default";
-    app.add_option("-f,--file", filename, "A help string");
-
-    CLI11_PARSE(app, argc, argv);
     return 0;
 }
 
