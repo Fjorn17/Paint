55d4a3cc90cc09adb2e68116745c0cc6bb5a61e1
he-lioenay <elioenay.perez@gmail.com>
2022-09-07 10:34:44 +0200

    Libreria CLI

---
+++ Paint/compiler.h
@@ -0,0 +1,8 @@
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
+
+
+
+
+
+#endif //!TOOLBAR_H
--- Paint/main.cpp
+++ Paint/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include "CLI/App.hpp"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+int main(){
+    CLI::App app{"App description"};
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    CLI11_PARSE(app, argc, argv);
+    return 0;
 }
 
 
