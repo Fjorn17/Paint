10153140b87dfe737f2721665fccc2f25013e8b3
Fjorn17 <fjorn17@gmail.com>
2022-09-07 10:14:53 +0200

    error point

--- Paint/shape.cpp
+++ Paint/shape.cpp
@@ -5,6 +5,6 @@ Point& Point::operator+(const Point& other) {
 	return suma;
 }
 Point& Point::operator-(const Point& other) {
-	auto resta = Point(p1.x + p2.x, p1.y + p2.y);
+	auto resta = Point(x + other.x, y + other.y);
 	return resta;
 }
